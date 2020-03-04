021a677b2c0f6abe01daa048c71317f63e864dc6
Miguel <yurib2134@gmail.com>
2020-03-04 16:09:22 -0500

    agregando visual estudio

---
+++ BusinessLayer/ClienteBusinness.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using DataLayer;
+
+namespace BusinessLayer
+{
+    public class BusinessLayer
+    {
+        public static bool ProbarConexion()
+        {
+            return ClienteData.ProbarConexion();
+        }
+    }
+    
+}
